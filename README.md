e5c90796f6a3e3ada2e8cc1a9865738588178238
Guangxuan-Xiao <xgx@mit.edu>
2022-11-21 00:15:49 +0000

    add activation scales, fakequant and smooth, and demo notebook.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SmoothQuant: Accurate and Efficient Post-Training Quantization for Large Language Models 
 
-**We expect to release the code in ~2 weeks. Stay tuned!**
+**We plan to gradually release the code in ~2 weeks. Stay tuned!**
 
 **If you are interested in getting updates, please sign up [here](https://forms.gle/YjYQQas5Hbqge1LH9) to get notified!**
 
@@ -11,9 +11,9 @@
 Large language models (LLMs) show excellent performance but are compute- and memory-intensive. Quantization can reduce memory and accelerate inference. However, for LLMs beyond 100 billion parameters, existing methods cannot maintain accuracy or have to rely on techniques that do not run efficiently on hardware. We propose SmoothQuant, a training-free, lightweight, and general-purpose post-training quantization (PTQ) solution to enable lossless 8-bit weight, 8-bit activation (W8A8) quantization for LLMs that can be implemented efficiently. We observe that systematic outliers appear at fixed activation channels. Based on the fact that weights are easy to quantize while activations are not, SmoothQuant smooths the activation outliers by migrating the quantization difficulty from activations to weights with a mathematically equivalent transformation. SmoothQuant enables an INT8 quantization of both weights and activations for all the GEMMs in LLMs, including OPT-175B, BLOOM-176B, and GLM-130B. SmoothQuant has better hardware efficiency than existing techniques using mixed-precision activation quantization or weight-only quantization. We demonstrate up to 1.56x speedup and 2x memory reduction for LLMs with negligible loss in accuracy. Thanks to the hardware-friendly design, we integrate SmoothQuant into FasterTransformer, a state-of-the-art LLM serving framework, and achieve faster inference speed with half the number of GPUs compared to FP16. Our work offers a turn-key solution that reduces hardware costs and democratizes LLMs.
 
 
-## Setup
+## Installation
 
-```
+```bash
 conda create -n smoothquant python=3.8
 conda activate smoothquant
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
@@ -21,3 +21,7 @@ pip install huggingface accelerate datasets
 
 python setup.py install
 ```
+
+## Example
+
+In `examples/smoothquant_opt_demo.ipynb`, we use OPT-13B as an example to demonstrate SmoothQuant can match the accuracy of FP16 and INT8 inference. Currently, we simulate INT8 inference with FP16 (`smoothquant/fake_quant.py`), i.e., fake quantization. We have implmented the real 8-bit quantization with INT8 CUTLASS GEMM kernels for both PyTorch and FasterTransformer. Please stay tuned for the release.
---
+++ smoothquant/fake_quant.py
@@ -0,0 +1,112 @@
+import torch
+from torch import nn
+from functools import partial
+
+
+def quantize_weight_per_channel_absmax(w, n_bits=8):
+    # w: (out_features, in_features)
+    scales = w.abs().max(dim=-1, keepdim=True)[0]
+    q_max = 2**(n_bits-1)-1
+    scales.clamp_(min=1e-5).div_(q_max)
+    w.div_(scales).round_().mul_(scales)
+    return w
+
+
+@torch.no_grad()
+def quantize_weight_per_tensor_absmax(w, n_bits=8):
+    # w: (out_features, in_features)
+    scales = w.abs().max()
+    q_max = 2**(n_bits-1)-1
+    scales.clamp_(min=1e-5).div_(q_max)
+    w.div_(scales).round_().mul_(scales)
+    return w
+
+
+@torch.no_grad()
+def quantize_activation_per_token_absmax(t, n_bits=8):
+    t_shape = t.shape
+    t.view(-1, t_shape[-1])
+    scales = t.abs().max(dim=-1, keepdim=True)[0]
+    q_max = 2**(n_bits-1)-1
+    scales.clamp_(min=1e-5).div_(q_max)
+    t.div_(scales).round_().mul_(scales)
+    return t
+
+
+@torch.no_grad()
+def quantize_activation_per_tensor_absmax(t, n_bits=8):
+    t_shape = t.shape
+    t.view(-1, t_shape[-1])
+    scales = t.abs().max()
+    q_max = 2**(n_bits-1)-1
+    scales.clamp_(min=1e-5).div_(q_max)
+    t.div_(scales).round_().mul_(scales)
+    return t
+
+
+class W8A8Linear(nn.Module):
+    def __init__(self, in_features, out_features, bias=True, act_quant='per_token', quantize_output=False):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+
+        self.register_buffer('weight', torch.randn(self.out_features,
+                                                   self.in_features, dtype=torch.float16, requires_grad=False))
+        if bias:
+            self.register_buffer('bias', torch.zeros(
+                (1, self.out_features), dtype=torch.float16, requires_grad=False))
+        else:
+            self.register_buffer('bias', None)
+
+        if act_quant == 'per_token':
+            self.act_quant_name = 'per_token'
+            self.act_quant = partial(
+                quantize_activation_per_token_absmax, n_bits=8)
+        elif act_quant == 'per_tensor':
+            self.act_quant_name = 'per_tensor'
+            self.act_quant = partial(
+                quantize_activation_per_tensor_absmax, n_bits=8)
+        else:
+            raise ValueError(f'Invalid act_quant: {act_quant}')
+
+        if quantize_output:
+            self.output_quant_name = self.act_quant_name
+            self.output_quant = self.act_quant
+        else:
+            self.output_quant_name = 'None'
+            self.output_quant = lambda x: x
+
+    def to(self, *args, **kwargs):
+        super(W8A8Linear, self).to(*args, **kwargs)
+        self.weight = self.weight.to(*args, **kwargs)
+        if self.bias is not None:
+            self.bias = self.bias.to(*args, **kwargs)
+        return self
+
+    @torch.no_grad()
+    def forward(self, x):
+        q_x = self.act_quant(x)
+        y = torch.functional.F.linear(q_x, self.weight, self.bias)
+        q_y = self.output_quant(y)
+        return q_y
+
+    @staticmethod
+    def from_float(module, weight_quant='per_channel', act_quant='per_token', quantize_output=False):
+        assert isinstance(module, torch.nn.Linear)
+        new_module = W8A8Linear(
+            module.in_features, module.out_features, module.bias is not None, act_quant=act_quant, quantize_output=quantize_output)
+        if weight_quant == 'per_channel':
+            new_module.weight = quantize_weight_per_channel_absmax(
+                module.weight, n_bits=8)  # use 8-bit integer for weight
+        elif weight_quant == 'per_tensor':
+            new_module.weight = quantize_weight_per_tensor_absmax(
+                module.weight, n_bits=8)
+        else:
+            raise ValueError(f'Invalid weight_quant: {weight_quant}')
+        new_module.weight_quant_name = weight_quant
+        if module.bias is not None:
+            new_module.bias = module.bias
+        return new_module
+
+    def __repr__(self):
+        return f'W8A8Linear({self.in_features}, {self.out_features}, bias={self.bias is not None}, weight_quant={self.weight_quant_name}, act_quant={self.act_quant_name}, output_quant={self.output_quant_name})'
