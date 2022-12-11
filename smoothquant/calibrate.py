2714dcdc3d4275675fedbc6bed8f8f07735dda47
Guangxuan Xiao <xgx@mit.edu>
2022-12-11 05:55:23 +0000

    update calibrate

--- smoothquant/calibrate.py
+++ smoothquant/calibrate.py
@@ -16,8 +16,11 @@ import functools
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def build_model_and_tokenizer(model_name, tokenizer):
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer, model_max_length=512)
+def build_model_and_tokenizer(model_name, tokenizer_name=None):
+    if tokenizer_name is None:
+        tokenizer_name = model_name
+    tokenizer = AutoTokenizer.from_pretrained(
+        tokenizer_name, model_max_length=512)
     kwargs = {"torch_dtype": torch.float16, "device_map": "auto"}
     model = AutoModelForCausalLM.from_pretrained(model_name, **kwargs)
     return model, tokenizer
@@ -80,7 +83,7 @@ def parse_args():
                         help='location of the calibration dataset, we use the validation set of the Pile dataset')
     parser.add_argument('--num-samples', type=int, default=512)
     parser.add_argument('--seq-len', type=int, default=512)
-    parser.add_argument('--tokenizer', type=str, default=None,
+    parser.add_argument('--tokenizer-name', type=str, default=None,
                         help='name of the tokenizer to use. By default, use the same as model-name')
     args = parser.parse_args()
     return args
@@ -89,13 +92,14 @@ def parse_args():
 @torch.no_grad()
 def main():
     args = parse_args()
-    tokenizer = args.tokenizer if args.tokenizer is not None else args.model_name
-    model, tokenizer = build_model_and_tokenizer(args.model_name, tokenizer)
+    model, tokenizer = build_model_and_tokenizer(
+        args.model_name, args.tokenizer_name)
 
     act_scales = calibrate(model, tokenizer, args.data_path,
                            args.num_samples, args.seq_len)
 
-    os.makedirs(os.path.dirname(os.path.abspath(args.output_path)), exist_ok=True)
+    os.makedirs(os.path.dirname(
+        os.path.abspath(args.output_path)), exist_ok=True)
     torch.save(act_scales, args.output_path)
 
 
--- smoothquant/prepare_for_fastertransformer.py
+++ smoothquant/prepare_for_fastertransformer.py
@@ -8,9 +8,9 @@ from transformers import AutoModelForCausalLM
 @torch.no_grad()
 def main():
     parser = ArgumentParser()
-    parser.add_argument("model", help="Name or path of non-smoothed model")
-    parser.add_argument("scales", help="Path of activation scales. Can be obtained with the calibrate.py script.")
-    parser.add_argument("output-path", help="Where to save the smoothed model")
+    parser.add_argument("--model", help="Name or path of non-smoothed model")
+    parser.add_argument("--scales", help="Path of activation scales. Can be obtained with the calibrate.py script.")
+    parser.add_argument("--output-path", help="Where to save the smoothed model")
     args = parser.parse_args()
 
     model = AutoModelForCausalLM.from_pretrained(args.model, torch_dtype=torch.float16, device_map='auto')
