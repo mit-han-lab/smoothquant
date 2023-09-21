ae775d9ba713a4d519a433442985343ab041b2d1
zhangpeng <zhangpeng156@meituan.com>
2023-09-21 16:00:16 +0800

    add int8 llama

--- smoothquant/calibration.py
+++ smoothquant/calibration.py
@@ -118,3 +118,82 @@ def get_static_decoder_layer_scales(model,
         decoder_layer_scales.append(scale_dict)
 
     return decoder_layer_scales, act_dict
+
+#TODO: merge to get_static_decoder_layer_scales method
+@torch.no_grad()
+def get_static_llama_decoder_layer_scales(model,
+                                          tokenizer,
+                                          dataset_path,
+                                          num_samples=512,
+                                          seq_len=512,
+                                          ):
+    model.eval()
+    device = next(model.parameters()).device
+
+    act_dict = defaultdict(dict)
+
+    def stat_io_hook(m, x, y, name):
+        if isinstance(x, tuple):
+            x = x[0]
+        if name not in act_dict or "input" not in act_dict[name]:
+            act_dict[name]["input"] = x.detach().abs().max().item()
+        else:
+            act_dict[name]["input"] = max(
+                act_dict[name]["input"], x.detach().abs().max().item())
+        if isinstance(y, tuple):
+            y = y[0]
+        if name not in act_dict or "output" not in act_dict[name]:
+            act_dict[name]["output"] = y.detach().abs().max().item()
+        else:
+            act_dict[name]["output"] = max(
+                act_dict[name]["output"], y.detach().abs().max().item())
+
+    hooks = []
+    for name, m in model.named_modules():
+        if isinstance(m, torch.nn.Linear):
+            hooks.append(m.register_forward_hook(
+                partial(stat_io_hook, name=name)))
+
+    print("Collecting activation scales...")
+    pbar = tqdm(range(num_samples))
+    dataset = load_dataset('json', data_files=dataset_path, split="train")
+    dataset = dataset.shuffle(seed=42)
+    for i in pbar:
+        input_ids = tokenizer(dataset[i]["text"], return_tensors="pt",
+                              max_length=seq_len, truncation=True).input_ids.to(device)
+        model(input_ids)
+        mean_scale = np.mean([v["input"] for v in act_dict.values()])
+        pbar.set_description(f"Mean input scale: {mean_scale:.2f}")
+    for hook in hooks:
+        hook.remove()
+
+    decoder_layer_scales = []
+    for idx in range(model.config.num_hidden_layers):
+        scale_dict = {}
+        # self attenion scales
+        scale_dict["attn_input_scale"] = act_dict[
+            f"model.layers.{idx}.self_attn.q_proj"]['input'] / 127
+        scale_dict["q_output_scale"] = act_dict[
+            f"model.layers.{idx}.self_attn.q_proj"]['output'] / 127
+        scale_dict["k_output_scale"] = act_dict[
+            f"model.layers.{idx}.self_attn.k_proj"]['output'] / 127
+        scale_dict["v_output_scale"] = act_dict[
+            f"model.layers.{idx}.self_attn.v_proj"]['output'] / 127
+        scale_dict["out_input_scale"] = act_dict[
+            f"model.layers.{idx}.self_attn.o_proj"]['input'] / 127
+        # mlp scales
+        scale_dict["gate_input_scale"] = act_dict[
+            f"model.layers.{idx}.mlp.gate_proj"]['input'] / 127
+        scale_dict["up_input_scale"] = act_dict[
+            f"model.layers.{idx}.mlp.up_proj"]["input"] / 127
+        scale_dict["down_input_scale"] = act_dict[
+            f"model.layers.{idx}.mlp.down_proj"]["input"] / 127
+        scale_dict["gate_output_scale"] = act_dict[
+            f"model.layers.{idx}.mlp.gate_proj"]['output'] / 127
+        scale_dict["up_output_scale"] = act_dict[
+            f"model.layers.{idx}.mlp.up_proj"]["output"] / 127
+        scale_dict["down_output_scale"] = act_dict[
+            f"model.layers.{idx}.mlp.down_proj"]["output"] / 127
+        decoder_layer_scales.append(scale_dict)
+
+    return decoder_layer_scales, act_dict
--- smoothquant/smooth.py
+++ smoothquant/smooth.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 
 from transformers.models.opt.modeling_opt import OPTDecoderLayer
 from transformers.models.bloom.modeling_bloom import BloomBlock
+from transformers.models.llama.modeling_llama import LlamaDecoderLayer, LlamaAttention, LlamaRMSNorm
 
 
 @torch.no_grad()
@@ -26,6 +27,29 @@ def smooth_ln_fcs(ln, fcs, act_scales, alpha=0.5):
     ln.weight.div_(scales)
     ln.bias.div_(scales)
 
+    for fc in fcs:
+        fc.weight.mul_(scales.view(1, -1))
+
+
+def smooth_ln_fcs_llama(ln, fcs, act_scales, alpha=0.5):
+    if not isinstance(fcs, list):
+        fcs = [fcs]  
+    assert isinstance(ln, LlamaRMSNorm) #llama use rmsnorm
+    for fc in fcs:
+        assert isinstance(fc, nn.Linear)
+        assert ln.weight.numel() == fc.in_features == act_scales.numel()
+    device, dtype = fcs[0].weight.device, fcs[0].weight.dtype
+    act_scales = act_scales.to(device=device, dtype=dtype)
+    weight_scales = torch.cat([fc.weight.abs().max(
+        dim=0, keepdim=True)[0] for fc in fcs], dim=0)
+    print(f"weight_scales shape: {weight_scales.shape}")
+    weight_scales = weight_scales.max(dim=0)[0].clamp(min=1e-5)
+    scales = (act_scales.pow(alpha) / weight_scales.pow(1-alpha)
+              ).clamp(min=1e-5).to(device).to(dtype)
+
+    print(f'smoothed scales:{scales}')
+    # do layer norm smooth
+    ln.weight.div_(scales)
     for fc in fcs:
         fc.weight.mul_(scales.view(1, -1))
 
@@ -54,3 +78,17 @@ def smooth_lm(model, scales, alpha=0.5):
             fc1 = module.mlp.dense_h_to_4h
             fc1_input_scales = scales[name + '.mlp.dense_h_to_4h']
             smooth_ln_fcs(ffn_ln, fc1, fc1_input_scales, alpha)
+        elif isinstance(module, LlamaDecoderLayer):
+            print(f"smooth llama decoder: {name}")
+            attn_ln = module.input_layernorm #attention forward norm
+            qkv = [module.self_attn.q_proj,
+                   module.self_attn.k_proj, module.self_attn.v_proj]
+            qkv_input_scales = scales[name + '.self_attn.q_proj']
+            smooth_ln_fcs_llama(attn_ln, qkv, qkv_input_scales, alpha)
+
+            ffn_ln = module.post_attention_layernorm #feed forward norm
+            fcs = [module.mlp.gate_proj, module.mlp.up_proj]
+            fcs_input_scales = scales[name + '.mlp.gate_proj']
+
+            smooth_ln_fcs_llama(ffn_ln, fcs, fcs_input_scales, alpha)
+        
