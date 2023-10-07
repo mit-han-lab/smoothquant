dbf28c560a23528b64d33fa8b7b948e9e58d55da
Quanfeng Li <liquanfeng7@foxmail.com>
2023-10-07 03:01:52 +0000

    llama-2-chat demo

--- smoothquant/smooth.py
+++ smoothquant/smooth.py
@@ -3,13 +3,14 @@ import torch.nn as nn
 
 from transformers.models.opt.modeling_opt import OPTDecoderLayer
 from transformers.models.bloom.modeling_bloom import BloomBlock
+from transformers.models.llama.modeling_llama import LlamaDecoderLayer, LlamaAttention, LlamaRMSNorm
 
 
 @torch.no_grad()
 def smooth_ln_fcs(ln, fcs, act_scales, alpha=0.5):
     if not isinstance(fcs, list):
         fcs = [fcs]
-    assert isinstance(ln, nn.LayerNorm)
+    assert isinstance(ln, (nn.LayerNorm, LlamaRMSNorm))
     for fc in fcs:
         assert isinstance(fc, nn.Linear)
         assert ln.weight.numel() == fc.in_features == act_scales.numel()
@@ -24,7 +25,8 @@ def smooth_ln_fcs(ln, fcs, act_scales, alpha=0.5):
               ).clamp(min=1e-5).to(device).to(dtype)
 
     ln.weight.div_(scales)
-    ln.bias.div_(scales)
+    if hasattr(ln, "bias"):
+        ln.bias.div_(scales)
 
     for fc in fcs:
         fc.weight.mul_(scales.view(1, -1))
@@ -53,4 +55,14 @@ def smooth_lm(model, scales, alpha=0.5):
             ffn_ln = module.post_attention_layernorm
             fc1 = module.mlp.dense_h_to_4h
             fc1_input_scales = scales[name + '.mlp.dense_h_to_4h']
+            smooth_ln_fcs(ffn_ln, fc1, fc1_input_scales, alpha)
+        elif isinstance(module, LlamaDecoderLayer):
+            attn_ln = module.input_layernorm
+            qkv = [module.self_attn.q_proj, module.self_attn.k_proj, module.self_attn.v_proj]
+            qkv_input_scales = scales[name + '.self_attn.q_proj']
+            smooth_ln_fcs(attn_ln, qkv, qkv_input_scales, alpha)
+
+            ffn_ln = module.post_attention_layernorm
+            fc1 = [module.mlp.gate_proj, module.mlp.up_proj]
+            fc1_input_scales = scales[name + '.mlp.up_proj']
             smooth_ln_fcs(ffn_ln, fc1, fc1_input_scales, alpha)
